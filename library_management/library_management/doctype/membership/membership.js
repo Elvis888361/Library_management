45f47d1e14caecb9816e28edc60ba4d83329cfa8
Elvis888361 <elvisndegwa90@gmail.com>
2024-09-11 14:44:27 +0300

    test2

--- library_management/library_management/doctype/membership/membership.js
+++ library_management/library_management/doctype/membership/membership.js
@@ -8,7 +8,7 @@ frappe.ui.form.on("Membership", {
 		});
 		frm.page.set_indicator(frm.doc.status, 'blue')
 	},
-	validate(frm){
+	after_save(frm){
 		frappe.call({
 			method: 'library_management.library_management.doctype.membership.membership.create_customer',
 			args: {
@@ -18,5 +18,23 @@ frappe.ui.form.on("Membership", {
 				frappe.msgprint(r.message)
 			}
 	});
-	}
+	},
+	date_of_birth(frm) {
+        const today = new Date();
+        const birth = new Date(frm.doc.date_of_birth);
+        
+        if (isNaN(birth)) {
+            frappe.msgprint(__('Invalid date of birth'));
+            return;
+        }
+
+        let age = today.getFullYear() - birth.getFullYear();
+        const monthDifference = today.getMonth() - birth.getMonth();
+        if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birth.getDate())) {
+            age--;
+        }
+
+        frm.set_value("age", age); 
+        frm.refresh_field("age");
+    }
 });
